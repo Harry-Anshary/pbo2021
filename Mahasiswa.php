b563b8f7b68b8bee081af976270b869eadd07a3d
Harry Anshary <harryanshary@student.untan.ac.id>
2021-02-18 19:53:27 +0700

    praktikum 02 membuat objek dan class

---
+++ Mahasiswa.php
@@ -0,0 +1,19 @@
+<?php
+
+Class Mahasiswa
+{
+    public $nim;
+    public $nama;
+    public $tanggal_lahir;
+    public $jenis_kelamin;
+
+    public function tampilkanAngkatan()
+    {
+    
+    }
+
+    public function tampilkanUmur()
+    {
+    
+    }
+}
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+include('Mahasiswa.php');
+ 
+$harry = new Mahasiswa();
+$irvan = new Mahasiswa();
+$zacky = new Mahasiswa();
+
+$harry->nim = 'H1101191040';
+$harry->nama = 'Harry Anshary';
+$harry->tanggal_lahir = '31 Maret 2001';
+$harry->jenis_kelamin = 'Laki - laki';
+
+$irvan->nim = 'H1101191062';
+$irvan->nama = 'Muhammad Pramudia Irvan Chaniago';
+$irvan->tanggal_lahir = '5 Mei 2001';
+$irvan->jenis_kelamin = 'Laki - laki';
+
+$zacky->nim = 'H1101191024';
+$zacky->nama = 'Hazacky Azwat Ramadhan';
+$zacky->tanggal_lahir = '23 November 2001';
+$zacky->jenis_kelamin = 'Laki - laki';
+
+
+
+
